5c8aed4ed05de52b3fd1646f8f237a981b4fd75d
Eric Son <urustin@naver.com>
2023-06-07 21:15:01 +1000

    070623

--- php/contact.php
+++ php/contact.php
@@ -6,7 +6,7 @@
  * ------------------------------------
  */
  
-$to    			= "test@surjithctly.in"; // <--- Your email ID here
+$to    			= "admin@flaresolution.com"; // <--- Your email ID here
 
 /*
  * ------------------------------------
@@ -14,21 +14,15 @@ $to    			= "test@surjithctly.in"; // <--- Your email ID here
  * ------------------------------------
  */
  
-$name     = $_POST["name"];
 $email    = $_POST["email"];
-$subject    = $_POST["subject"];
+$subject  = "Website Request";
+$msg      = "";
 $website  = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-if (isset($email) && isset($name)) {
+if (isset($email)) {
 	$headers  = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-	$headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
-	$msg      = 'Hello Admin, <br/> <br/> Here are the message details:';
-	$msg     .= ' <br/> <br/> <table border="1" cellpadding="6" cellspacing="0" style="border: 1px solid  #eeeeee;">';
-	foreach ($_POST as $label => $value) {
-	    $msg .= "<tr><td width='100'>". ucfirst($label) . "</td><td width='300'>" . $value . " </tr>";
-	}
-	$msg      .= " </table> <br> --- <br>This e-mail was sent from $website";
+	$headers .= "From: " . $email . "\r\n"."Reply-To: " . $email . "\r\n" ;
 	 
 	$mail =  mail($to, $subject, $msg, $headers);
 
